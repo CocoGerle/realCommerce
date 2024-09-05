85fa86116045ed93afa0fb73efb8cc2f4dd53ede
LPC243012 LPC243012 <LPC243012@G301-CC0201.local>
2024-09-05 12:49:51 +0800

    g

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { FiSearch, FiHeart, FiShoppingCart } from "react-icons/fi";
+
+export const Header = () => {
+  return (
+    <header className="bg-black py-[16px] px-[24px] flex justify-between text-white">
+      <div className="flex gap-[16px] items-center">
+        <Image
+          width={32}
+          height={32}
+          src={"/Pinecone.png"}
+          alt="Pinecone logo"
+        />
+        <p className="">Бүтээгдэхүүн</p>
+      </div>
+      <div className="bg-[#18181B] flex gap-2 items-center py-[8px] px-[16px] rounded-md w-[300px]">
+        <FiSearch size={24} />
+        <input
+          placeholder="Бүтээгдэхүүн  хайх"
+          type="search"
+          className="bg-transparent text-[#71717A]"
+        />
+      </div>
+      <div className="flex gap-[24px] items-center">
+        <FiHeart size={24} />
+        <FiShoppingCart size={24} />
+        <div className="flex gap-2">
+          <button className="border border-[#2563EB] rounded-md py-[8px] px-[12px]">
+            Бүртгүүлэх
+          </button>
+          <button className="bg-[#2563EB] rounded-md py-[8px] px-[12px]">
+            Нэвтрэх
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
